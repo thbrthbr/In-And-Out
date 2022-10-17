fe4f1ccf578947fe7cfe464046df6cfba6faf907
1z3r091 <1z3r091@gmail.com>
2022-10-17 18:15:37 +0900

    FEAT/initial routing component

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,27 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Login from "./components/login/Login";
+import Calendar from "./components/calendar/Calendar";
+import Setting from "./components/setting/Setting";
+import Inout from "./components/inout/Inout";
+import Report from "./components/report/Report";
+import Error from "./components/Error";
+
+// import LoginSharedLayout from "./components/layout/LoginSharedLayout";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>First Route</div>}></Route>
+        {/* <Route path="/" element={<LoginSharedLayout />}> */}
+        <Route path="/login" element={<Login />}></Route>
+        {/* </Route> */}
+        <Route path="calendar" element={<Calendar />}></Route>
+        <Route path="setting" element={<Setting />}></Route>
+        <Route path="inout" element={<Inout />}></Route>
+        <Route path="report" element={<Report />}></Route>
+        <Route path="*" element={<Error />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/components/inout/Inout.js
@@ -0,0 +1,3 @@
+export default function Inout() {
+  return <div>Inout</div>;
+}
