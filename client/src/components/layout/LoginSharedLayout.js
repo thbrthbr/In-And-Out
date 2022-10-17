c3a98f677e844d6e8713de327f7312d7ee4d1511
1z3r091 <1z3r091@gmail.com>
2022-10-17 21:43:51 +0900

    REFACTOR/main view components into main folder

--- client/src/components/layout/LoginSharedLayout.js
+++ client/src/components/layout/LoginSharedLayout.js
@@ -1,11 +1,9 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 export default function LoginSharedLayout() {
   return (
     <>
       <div>LoginSharedLayout(헤더)</div>
-
-      {/* <Link to="/login">Login</Link> */}
       <Outlet />
     </>
   );
---
+++ client/src/components/setting/Signout.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+export default function Signout() {
+  return (
+    <>
+      <div>Signout</div>
+      <Link to="/setting/password_change">PasswordChange</Link>
+      <br />
+      <Link to="/setting">ProfileChange</Link>
+      <br />
+      <Link to="/calendar">Home</Link>
+    </>
+  );
+}
