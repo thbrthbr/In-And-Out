58a8e8aa07665afc2dd05af93f3c3bf09c8b1835
jiyongYoon <jyyoon0615@naver.com>
2022-10-17 15:08:04 +0900

    CHORE/init setting
    - 프로젝트 빌드

---
+++ server/src/test/java/com/rezero/inandout/InandoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rezero.inandout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InandoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
