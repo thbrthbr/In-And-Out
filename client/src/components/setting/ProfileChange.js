00e80e1a561c54d8cc58830d3469f31e25dccbf0
thbrthbr <th13466@naver.com>
2022-10-18 21:46:29 +0900

    FEAT/settings

--- client/src/components/setting/ProfileChange.js
+++ client/src/components/setting/ProfileChange.js
@@ -1,14 +1,69 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export default function ProfileChange() {
+  const placeholders = [
+    { placeholder: "이메일" },
+    { placeholder: "닉네임" },
+    { placeholder: "전화번호" },
+    { placeholder: "생년월일" },
+    { placeholder: "거주지" },
+    { placeholder: "성별" },
+  ];
+
   return (
-    <div>
-      <div>ProfileChange</div>
-      <Link to="/password_change">PasswordChange</Link>
-      <br />
-      <Link to="/signout">Signout</Link>
-      <br />
-      <Link to="/calendar">Home</Link>
-    </div>
+    <>
+      <Page>
+        {placeholders.map((list) => (
+          <TextInput>
+            <input
+              placeholder={list.placeholder}
+              style={{
+                padding: 0,
+                border: "none",
+                fontSize: "30px",
+                width: "500px",
+                height: "40px",
+              }}
+              type="text"
+            />
+          </TextInput>
+        ))}
+
+        <ButtonInput>
+          <button
+            style={{
+              fontSize: "30px",
+
+              width: "500px",
+              height: "40px",
+            }}
+          >
+            제출
+          </button>
+        </ButtonInput>
+      </Page>
+    </>
   );
 }
+
+const Page = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const TextInput = styled.div`
+  width: 500px;
+  height: 100px;
+
+  display: flex;
+  align-items: center;
+`;
+
+const ButtonInput = styled.div`
+  width: 500px;
+  height: 100px;
+  display: flex;
+  align-items: center;
+`;
