6e5db807f6b4762ac4fb8ba60ff4a4e288cdca8d
1z3r091 <1z3r091@gmail.com>
2022-10-20 19:20:54 +0900

    FEAT/report 화면에 그래프 추가

--- client/src/components/main/Report.js
+++ client/src/components/main/Report.js
@@ -1,6 +1,39 @@
-import { Link, NavLink } from "react-router-dom";
+import { useRef, useEffect } from "react";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import styled from "styled-components";
 
+import {
+  Chart,
+  BarElement,
+  BarController,
+  CategoryScale,
+  LinearScale,
+  Title,
+  Legend,
+  DoughnutController,
+  ArcElement,
+  PieController,
+  LineController,
+  PointElement,
+  LineElement,
+} from "chart.js";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+
+Chart.register(
+  BarElement,
+  BarController,
+  CategoryScale,
+  LinearScale,
+  Title,
+  Legend,
+  DoughnutController,
+  ArcElement,
+  PieController,
+  ChartDataLabels,
+  LineController,
+  PointElement,
+  LineElement
+);
 const SideButton = styled.div`
   width: 180px;
   height: 50px;
@@ -15,7 +48,104 @@ const NavLinkContainer = styled.div`
   display: flex;
 `;
 
+const drawChart = (ctx, config) => {
+  return new Chart(ctx, config);
+};
+
 export default function Report() {
+  const canvasRef = useRef(null);
+  const loc = useLocation();
+  // const [data, setData] = useState([]);
+  let data = [];
+  let chartType = "doughnut"; // default: doghnut, checkbox에 따라 변함 - state로 관리 해야될듯
+  let labels = [];
+
+  if (loc.pathname === "/report/monthly") {
+    data = [10, 20, 15, 5, 50];
+    labels = [
+      "주거 - 10% 100,000",
+      "주거 - 10% 100,000",
+      "주거 - 10% 100,000",
+      "주거 - 10% 100,000",
+      "주거 - 10% 100,000",
+    ];
+  } else if (loc.pathname === "/report/yearly") {
+    data = [20, 30, 10, 30, 10];
+    labels = [
+      "주거 - 20% 200,000",
+      "주거 - 20% 200,000",
+      "주거 - 20% 200,000",
+      "주거 - 20% 200,000",
+      "주거 - 20% 200,000",
+    ];
+  }
+
+  const config = {
+    type: chartType,
+    data: {
+      datasets: [
+        {
+          data: data,
+          backgroundColor: [
+            "rgb(255, 99, 132)",
+            "rgb(255, 159, 64)",
+            "rgb(255, 205, 86)",
+            "rgb(75, 192, 192)",
+            "rgb(54, 162, 235)",
+          ],
+        },
+      ],
+      labels: labels,
+    },
+    options: {
+      responsive: false,
+      tooltips: {
+        callbacks: {
+          label: function (tooltipItem, data) {
+            var dataset = data.datasets[tooltipItem.datasetIndex];
+            var total = dataset.data.reduce(function (
+              previousValue,
+              currentValue,
+              currentIndex,
+              array
+            ) {
+              return previousValue + currentValue;
+            });
+            var currentValue = dataset.data[tooltipItem.index];
+            var precentage = Math.floor((currentValue / total) * 100 + 0.5);
+            return precentage + "%";
+          },
+        },
+      },
+      plugins: {
+        datalabels: {
+          formatter: (value) => {
+            if (value < 1) return "";
+            return `${value} %`;
+          },
+        },
+        legend: {
+          display: true,
+          position: "right",
+          align: "start",
+        },
+      },
+    },
+  };
+
+  useEffect(() => {
+    let charId;
+    if (canvasRef.current) {
+      // if (chartRef.current) drawChart(chartRef.current, "hi");
+      // const ctx = document.getElementById("myChart");
+
+      charId = drawChart(canvasRef.current, config);
+    }
+    return () => {
+      charId.destroy();
+    };
+  });
+
   return (
     <div>
       <NavLinkContainer>
@@ -43,6 +173,7 @@ export default function Report() {
           <SideButton>{"연간 보고서"}</SideButton>
         </NavLink>
       </NavLinkContainer>
+
       <div>Report</div>
       <Link to="/profile_change">Setting</Link>
       <br />
@@ -51,6 +182,9 @@ export default function Report() {
       <Link to="/inout">Inout</Link>
       <br />
       <Link to="/">Logout</Link>
+
+      <input type="checkbox"></input>
+      <canvas id="myChart" width={1000} height={500} ref={canvasRef}></canvas>
     </div>
   );
 }
