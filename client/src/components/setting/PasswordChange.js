3491006a1b60b583a47aae2a4d06e2917f9a4f8a
thbrthbr <th13466@naver.com>
2022-10-27 20:55:28 +0900

    FEAT/ 프로필변경&비밀번호변경&회원탈퇴 기능 구현

--- client/src/components/setting/PasswordChange.js
+++ client/src/components/setting/PasswordChange.js
@@ -2,21 +2,11 @@ import styled from "styled-components";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { recoverInitiateSchema } from "../../schema/form_validation";
+import { loginStore } from "../../store/store.js";
 
 export default function PasswordChange() {
-  // const [button, setButton] = useState();
-
-  // const placeholders = [
-  //   { placeholder: "이전 비밀번호" },
-  //   { placeholder: "새 비밀번호" },
-  //   { placeholder: "새 비밀번호 확인" },
-  // ];
-  // const valueIds = [
-  //   { valueId: "이전 비밀번호" },
-  //   { valueId: "새 비밀번호" },
-  //   { valueId: "새 비밀번호 확인" },
-  // ];
-  //나중에 form으로 데이터 보낼 때 쓸 input id들
+  const { id, nickname, phoneNumber, birthdate, residence, gender } =
+    loginStore();
 
   const {
     register,
@@ -26,8 +16,44 @@ export default function PasswordChange() {
     resolver: yupResolver(recoverInitiateSchema),
   });
 
-  const onSubmit = (data) => {
-    alert("submit");
+  async function put(e) {
+    const response = await fetch(`http://localhost:4000/users/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        password: e,
+        nickname: nickname,
+        phoneNumber: phoneNumber,
+        birthdate: birthdate,
+        residence: residence,
+        gender: gender,
+      }),
+    });
+
+    console.log(response);
+
+    if (response.ok) {
+      alert("수정완료");
+    } else {
+      alert("오류");
+    }
+  }
+
+  const onSubmit = async (e) => {
+    const response = await fetch(`http://localhost:4000/users`);
+
+    if (response.ok) {
+      const users = await response.json();
+      const user = users.find((user) => user.id === id);
+      if (user.password !== e["oldPw"]) {
+        alert("기존 비밀번호와 일치하지 않습니다.");
+        throw new Error("아이디 또는 비밀번호가 일치하지 않습니다.");
+      }
+    } else {
+      throw new Error("서버 통신이 원할하지 않습니다.");
+    }
+    await put(e["pw"]);
+    window.location.reload();
   };
 
   return (
@@ -45,6 +71,7 @@ export default function PasswordChange() {
                 height: "30px",
               }}
               type="password"
+              {...register("oldPw")}
             />
           </TextInput>
           <TextInput>
