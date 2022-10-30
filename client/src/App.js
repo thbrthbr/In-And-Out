894fe6a14f2ddbe09e135748e8bcf9fc9d7edd10
thbrthbr <th13466@naver.com>
2022-10-30 20:20:47 +0900

    FIX/프로필변경 일부 수정 + 폰트 추가

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,8 @@ import Calendar from "./components/main/Calendar";
 // import PasswordChange from "./components/setting/PasswordChange";
 // import Signout from "./components/setting/Signout";
 
+import styled from "styled-components";
+
 import Inout from "./components/main/Inout";
 import Report from "./components/main/Report";
 import Error from "./components/Error";
@@ -24,6 +26,7 @@ import Layout from "./components/common/Layout";
 import Screen from "./components/common/Screen";
 
 import { QueryClientProvider, QueryClient } from "react-query";
+
 // import { createStore } from "redux";
 
 // import { Provider, useSelector, useDispatch, connect } from "react-redux";
@@ -43,28 +46,35 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Login />}></Route>
-            <Route path="/signin" element={<Signin />}></Route>
-            <Route path="/identify_email" element={<IdentifyEmail />}></Route>
-            <Route path="/identify_phone" element={<IdentifyPhone />}></Route>
-            <Route path="/initiate" element={<RecoverInitiate />}></Route>
-            <Route path="/calendar" element={<Calendar />}></Route>
-            <Route path="/report/monthly" element={<Report />}></Route>
-            <Route path="/report/yearly" element={<Report />}></Route>
-            <Route path="/inout/income" element={<Inout />}></Route>
-            <Route path="/inout/expense" element={<Inout />}></Route>
-            <Route path="/profile_change" element={<Screen />}></Route>
-            <Route path="/password_change" element={<Screen />}></Route>
-            <Route path="/signout" element={<Screen />}></Route>
-            <Route path="*" element={<Error />}></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Font>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Login />}></Route>
+              <Route path="/signin" element={<Signin />}></Route>
+              <Route path="/identify_email" element={<IdentifyEmail />}></Route>
+              <Route path="/identify_phone" element={<IdentifyPhone />}></Route>
+              <Route path="/initiate" element={<RecoverInitiate />}></Route>
+              <Route path="/calendar" element={<Calendar />}></Route>
+              <Route path="/report/monthly" element={<Report />}></Route>
+              <Route path="/report/yearly" element={<Report />}></Route>
+              <Route path="/inout/income" element={<Inout />}></Route>
+              <Route path="/inout/expense" element={<Inout />}></Route>
+              <Route path="/profile_change" element={<Screen />}></Route>
+              <Route path="/password_change" element={<Screen />}></Route>
+              <Route path="/signout" element={<Screen />}></Route>
+              <Route path="*" element={<Error />}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Font>
     </QueryClientProvider>
   );
 }
 
 export default App;
+
+const Font = styled.div`
+  font-family: "OTWelcomeRA";
+  line-height: 40px;
+`;
