f5a132a1373dc61b18a91461828aa11543b6c1d2
1z3r091 <1z3r091@gmail.com>
2022-10-30 16:37:48 +0900

    REFACTOR/Loader 크기 및 위치 변경

--- client/src/components/main/Inout.js
+++ client/src/components/main/Inout.js
@@ -276,7 +276,19 @@ export default function Inout() {
     // newRowData = filteredRow;
   }
 
-  if (isLoading) return <PacmanLoader color="#36d7b7" />;
+  if (isLoading)
+    return (
+      <PacmanLoader
+        style={{
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+        color="#36d7b7"
+        size={50}
+      />
+    );
   console.log(isLoading, data);
   // setRows(data);
   return (
--- client/src/components/main/Report.js
+++ client/src/components/main/Report.js
@@ -360,7 +360,19 @@ export default function Report() {
     () => getData(API_URL, params)
   );
 
-  if (isLoading) return <PacmanLoader color="#36d7b7" />;
+  if (isLoading)
+    return (
+      <PacmanLoader
+        style={{
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+        color="#36d7b7"
+        size={50}
+      />
+    );
   setData();
 
   return (
